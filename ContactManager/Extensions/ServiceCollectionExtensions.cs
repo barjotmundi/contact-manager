a2ee656bd326658849aa1195679d8bb9f32aee61
barjot <barjo@formidabledesigns.com>
2026-01-30 00:28:36 -0700

    Added Controller #4

---
+++ ContactManager/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ContactManager.Repositories;
+using ContactManager.Services;
+namespace ContactManager.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddContactManagerServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IContactRepository, InMemoryContactRepository>();
+            services.AddSingleton<IContactService, ContactService>();
+            return services;
+        }
+    }
+}
