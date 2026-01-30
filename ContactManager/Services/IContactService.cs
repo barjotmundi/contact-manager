97e7fe9d4f6e59363740b69961ce9244d778d8ec
Barjot Mundi <139416025+barjotmundi@users.noreply.github.com>
2026-01-30 00:09:37 -0700

    Added Services #3 (#8)
    Close #3

---
+++ ContactManager/Services/IContactService.cs
@@ -0,0 +1,16 @@
+﻿using ContactManager.Models;
+using ContactManager.Dtos;
+using ContactManager.Utilities;
+
+namespace ContactManager.Services
+{
+    public interface IContactService
+    {
+        IEnumerable<ContactDto> GetAll();
+        ContactDto? GetById(Guid id);
+        OperationResult Add(ContactDto contact);
+        OperationResult Update(Guid id, ContactDto contact);
+        OperationResult Delete(Guid id);
+        IEnumerable<ContactDto> Search(string query);
+    }
+}
