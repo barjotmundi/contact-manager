8333cf65d4e64a08c8f748afcae9aa79d8aafd40
barjot <barjo@formidabledesigns.com>
2026-01-29 23:01:53 -0700

    Create Contact Model #2

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public required string Phone { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
